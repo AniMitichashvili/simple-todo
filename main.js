3b3e049827f4e7d8ec413a6b538bcfc940318633
ani-mitichashvili <145356727+ani-mitichashvili@users.noreply.github.com>
2025-02-17 18:40:04 +0400

    Fix status update

--- main.js
+++ main.js
@@ -37,30 +37,36 @@ class Todo {
     }
 }
 
-class TodoService
-{
+class TodoService {
     todos = [];
-    constructor(){}
+    constructor() { }
 
-    updateTodoText(newText,id){
+    updateTodoText(newText, id) {
         var elem = this.todos.find(item => item.id == id.split('id-')[1]);
         elem.text = newText;
     }
 
-    updateTodoStatus(newStatus,id){
+    updateTodoStatus(newStatus, id) {
         var elem = this.todos.find(item => item.id == id.split('id-')[1]);
         elem.status = newStatus;
     }
 
-    removeTodo(id){
+    removeTodo(id) {
         var elemIndex = this.todos.findIndex(item => item.id == id.split('id-')[1]);
         this.todos.splice(elemIndex, 1);
+    }
+
+    filterByText(text) {
+        if (text.length > 0)
+            return this.todos.filter(item => item.text.includes(text));
+
+        return this.todos;
     }
 }
 
 class HTMLWorker {
     generateTodoHtml(todo) {
-        return `<li class="list-group-item">
+        return `<li class="list-group-item ${todo.status}">
                     <div class="d-flex justify-content-between align-items-center">
                         <span>${todo.text}</span>
                         <div id='id-${todo.id}'>
@@ -75,10 +81,19 @@ class HTMLWorker {
     setHtmlToListElement(todo, container) {
         container.innerHTML += this.generateTodoHtml(todo);
     }
+
+    changeTodoStatusTo(todoLiItem, status) {
+        todoLiItem.className = `list-group-item ${status}`
+    }
+
+    generateTodos(todods, container) {
+        container.innerHTML = "";
+        todods.forEach(item => {
+            this.setHtmlToListElement(item, container);
+        });
+    }
 }
 
-
-var htmlWorker = new HTMLWorker();
 const todoInp = document.querySelector("#todo");
 const saveBtn = document.querySelector("#save");
 const todosList = document.querySelector(".todos-list");
@@ -86,7 +101,9 @@ var modalOpenBtn = document.querySelector("#modalOpener");
 const updateTodoInp = document.querySelector("#updateTodo");
 const statusSelect = document.querySelector("#status");
 const updateBtn = document.querySelector("#update")
+const searchTodosInp = document.querySelector("#search");
 var todoService = new TodoService();
+var htmlWorker = new HTMLWorker();
 
 
 saveBtn.addEventListener("click", function () {
@@ -94,32 +111,38 @@ saveBtn.addEventListener("click", function () {
     todoService.todos.push(todo);
     htmlWorker.setHtmlToListElement(todo, todosList);
     todoInp.value = "";
-})
+});
 
 updateBtn.addEventListener("click", function () {
     var todoNewText = updateTodoInp.value;
+    var newStatus = statusSelect.value;
 
     var todoElem = document.querySelector(`#${this.id}`);
-    todoElem.previousSibling.previousSibling.textContent = todoNewText;
+    if (todoNewText.length > 0)
+        todoElem.previousSibling.previousSibling.textContent = todoNewText;
 
-    todoService.updateTodoText(todoNewText,this.id)
+    htmlWorker.changeTodoStatusTo(todoElem.parentNode.parentNode, newStatus);
+    todoService.updateTodoText(todoNewText, this.id);
+    todoService.updateTodoStatus(newStatus, this.id)
 
-    updateTodo.value = "";
+    updateTodoInp.value = "";
+});
+
+searchTodosInp.addEventListener("input", function () {
+    var filteredTodos = todoService.filterByText(this.value);
+    htmlWorker.generateTodos(filteredTodos, todosList);
 })
 
 function doneTodo(event) {
     var parentElem = event.target.parentNode.parentNode.parentNode;
-    parentElem.classList.add('done');
-
-    todoService.updateTodoStatus("todo",event.target.parentNode.id)
+    htmlWorker.changeTodoStatusTo(parentElem, "done");
+    todoService.updateTodoStatus("done", event.target.parentNode.id)
 }
-
 
 function removeTodo(event) {
     var liTag = event.target.parentNode.parentNode.parentNode;
     var ulTag = liTag.parentNode;
     ulTag.removeChild(liTag);
-
     todoService.removeTodo(event.target.parentNode.id)
 }
 
